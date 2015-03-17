93c0af978c2a7829bd2a7c4694008444201f3d2f
hdvegasilva <hdvegasilva@gmail.com>
2015-03-17 11:51:43 -0400

    add files

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -4,21 +4,64 @@ import "fmt"
 
 // Salutation is an exported type.
 type Salutation struct {
-	name     string
-	greeting string
+	Name     string
+	Greeting string
 }
 
 // Printer is an exported type.
 type Printer func(string)
 
 // Greet is an exported function.
-func Greet(salutation Salutation, do Printer, isFormal bool) {
-	message, alternate := CreateMessage(salutation.name, salutation.greeting)
-	if isFormal {
-		do(message)
+func Greet(salutation Salutation, do Printer, isFormal bool, times int) {
+	message, alternate := CreateMessage(salutation.Name, salutation.Greeting)
+	i := 0
+	for {
+		if i >= times {
+			break
+		}
+
+		if i%2 == 0 {
+			i++
+			continue
+		}
+
+		if prefix := GetPrefix(salutation.Name); isFormal {
+			do(prefix + message)
+		} else {
+			do(alternate)
+		}
+		i++
+	}
+}
+
+// GetPrefix is an exported function.
+func GetPrefix(name string) (prefix string) {
+	switch {
+	case name == "Bob":
+		prefix = "Mr. "
+	case name == "Joe", name == "Amy", len(name) == 10:
+		prefix = "Dr. "
+	case name == "Mary":
+		prefix = "Mrs. "
+	default:
+		prefix = "Dude "
 	}
 
-	do(alternate)
+	return
+}
+
+// TypeSwitchTest is an exported function.
+func TypeSwitchTest(x interface{}) {
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case Salutation:
+		fmt.Println("salutation")
+	default:
+		fmt.Println("unknown")
+	}
 }
 
 // CreateMessage is an exported function.
--- hello.go
+++ hello.go
@@ -3,7 +3,6 @@ package main
 import "./greeting"
 
 func main() {
-
-	var s = Salutation{"Bob", "Hello"}
-	Greet(s, CreatePrintCustom("?"), false)
+	var s = greeting.Salutation{"Bob", "Hello"}
+	greeting.Greet(s, greeting.CreatePrintCustom("?"), true, 6)
 }
